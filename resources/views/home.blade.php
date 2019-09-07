c3ad029a7ca23ca1631a9bd284bfe71f30b7faa3
faisal-sameer <43131907+faisal-sameer@users.noreply.github.com>
2019-09-07 23:06:55 +0300

    home card test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
+
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
+
+
+
+
+
+
+
+
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -21,6 +37,34 @@
                     You are logged in!
                 </div>
             </div>
+            <!--      -->
+            <div class="container">
+                <div class="row">
+                    <!--basic-->
+                    
+                    <div class="col s12 m4">
+                        <div class="card">
+                            <div class="card-content">
+                                <div class="card-title green white-text">
+                                    <h3>Basic</h3>
+                                    <h5>$29/month</h5>
+                                </div>
+                                <ul class="collection">
+                                    <li class="collection-item">25GB Disk Space</li>
+                                    <li class="collection-item">30 Email Address</li>
+                                    <li class="collection-item">1 Domain</li>
+                                    <li class="collection-item">Live Support</li>
+                                    <li class="collection-item">Unlimited Bandwidth</li>
+                                </ul>
+                            </div>
+                            
+                            <div class="card-action">
+                                <div class="btn btn-block btn-large pink">Sing Up</div>
+                            </div>
+                        </div>
+                    </div>
+
+
         </div>
     </div>
 </div>
