e8c1a098c7fd8f638bfb9eb1716754462d1b40be
abdulaziz-al <abdulaziz_AF@github.com>
2019-09-07 23:10:56 +0300

    me card home test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,8 +18,29 @@
                     Please check your Email and active your account !!
                     @endif
 
-                    You are logged in!
+
+
+                    
                 </div>
+                
+            </div>
+            
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
+
+                
+            <div class="container">
+            <h3>A demo of Bootstrap Card</h3>
+            <div class="card" style="width: 20rem;">
+              <img class="card-img-top" src="iconend.png" alt="the image alt text here">
+              <div class="card-body">
+                <h5 class="card-title text-primary">Bootstrap 4</h5>
+                <p class="card-text">The great way to build mobile first web applications. Create responsive websites by built-in capabilities by utlizing 
+                variety of components with a powerful grid system.
+                </p>
+                <a href="#" class="btn btn-outline-dark">Learn more</a>
+              </div>
+            </div>
+            
             </div>
         </div>
     </div>
