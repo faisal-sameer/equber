282eeab9976e3aece757dfa872e9ae98cd01302b
abdulaziz-al <abdulaziz_AF@github.com>
2019-09-08 02:12:54 +0300

    cards css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -33,63 +33,6 @@
 
 
 
-<div class="container">
-    <div class="row">
-        <!--basic
-                            <img src="فهرس.png">
-
-                        -->
-        
-        <div class="col s12 m4">
-            <div class="card">
-                <div class="card-content">
-                    <div class="card-title green white-text">
-                        <h3>صاحب معدات</h3>
-                        
-                    </div>
-                    <ul class="collection">
-                        <li class="collection-item">سجل دخولك كصاحب معدة لتسهل تواصلك مع المهندسين </li>
-                      
-                    </ul>
-                </div>
-                
-                <div class="card-action">
-                    <div class="btn btn-block btn-large pink">Sing Up</div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-
-
-
-
-<div class="container">
-    <div class="row">
-        <!--basic
-                            <img src="iconend.png">
--->
-        
-        <div class="col s12 m4">
-            <div class="card">
-                <div class="card-content">
-                    <div class="card-title green white-text">
-                        <h3>مهندس</h3>
-                        
-                    </div>
-                    <ul class="collection">
-                        <li class="collection-item">سجل دخولك كمهندس حيث نسهل التواصل بينك وبين أصحاب معدات البناء</li>
-
-                    </ul>
-                </div>
-                
-                <div class="card-action">
-                    <div class="btn btn-block btn-large pink">Sing Up</div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
 
 </div>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,99 +1,78 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
+@section('content')
 
-        <title>Laravel</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+<div class="containe">
+    <div class="row justify-content-center">
 
-            .full-height {
-                height: 100vh;
-            }
+<div class="container">
+    <div class="row">
+        <!--basic
+                           
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
+                        -->
+        
+        <div class="col s12 m4">
+            <div class="card">
+                <div class="card-content">
+                    <div class="card-title green white-text">
+                        <h3>صاحب معدات</h3>
+                        
+                    </div>
+                    <ul class="collection">
+                        <li class="collection-item">سجل دخولك كصاحب معدة لتسهل تواصلك مع المهندسين </li>
+                      
+                    </ul>
                 </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <img class="pic" src="فهرس.png">
+                <div class="card-action">
+                    <a href="#" id="pic1" class="btn btn-block btn-large green"> إنشاء حساب </a>
                 </div>
             </div>
         </div>
-    </body>
-</html>
+    </div>
+</div>
+
+
+
+
+<div class="container">
+    <div class="row">
+        <!--basic
+                          
+-->
+        
+        <div class="col s12 m4">
+            <div class="card">
+                <div class="card-content">
+                    <div class="card-title green white-text">
+                        <h3>مهندس</h3>
+                        
+                    </div>
+                    <ul class="collection">
+                        <li class="collection-item">سجل دخولك كمهندس حيث نسهل التواصل بينك وبين أصحاب معدات البناء</li>
+
+                    </ul>
+                </div>
+                <img class="pic" src="iconend.png">
+                <div class="card-action">
+                    
+                    <a href="#" id="pic1" class="btn btn-block btn-large blue">إنشاء حساب </a>
+                    </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+    </div>
+</div>
+
+
+
+
+
+
+
+
+@endsection
