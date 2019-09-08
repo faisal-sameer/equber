ea56f49e4eb2eeb92ee110e37c04483504e0af60
abdulaziz-al <abdulaziz_AF@github.com>
2019-09-08 22:36:52 +0300

    save

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,11 @@
 @extends('layouts.app')
+@section('head')
+
+
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+@endsection
 @section('content')
 <!--
 <div class="container">
